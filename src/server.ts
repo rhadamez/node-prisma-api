dacf73f541019f24f7bc9eabc178fab1b16c4070
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2023-01-24 16:21:11 -0300

    feat: add courses migration

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log('affe!')
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const result = await prisma.courses.create({
+    data: {
+      duration: 420,
+      name: 'Java com Spring',
+      description: 'Conceitos iniciais do spring boot',
+      created_at: new Date()
+    }
+  })
+
+  console.log(result)
+}
+
+main()
